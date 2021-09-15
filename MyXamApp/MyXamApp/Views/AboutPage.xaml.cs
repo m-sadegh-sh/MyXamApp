7f9eceec0160cfc89aa51ba373830d0a273e55fb
Mohammad Sadegh Shad <m.sadegh.sh@gmail.com>
2021-09-15 13:12:58 +0430

    Add project files.

---
+++ MyXamApp/MyXamApp/Views/AboutPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.ComponentModel;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace MyXamApp.Views
+{
+    public partial class AboutPage : ContentPage
+    {
+        public AboutPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ MyXamApp/MyXamApp/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using MyXamApp.ViewModels;
+using System.ComponentModel;
+using Xamarin.Forms;
+
+namespace MyXamApp.Views
+{
+    public partial class ItemDetailPage : ContentPage
+    {
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+            BindingContext = new ItemDetailViewModel();
+        }
+    }
+}
